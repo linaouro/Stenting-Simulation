acbf81843504a3bdb5f3987be609bd09fdd9e1e6
Metadelee <lina.gundelwein@yahoo.de>
2017-05-10 16:04:09 -0400

    "update constructor to set centerline, radii and stenosis for left, right PA and PT"

--- Artery.m
+++ Artery.m
@@ -7,8 +7,7 @@ classdef Artery
         radii_st_dev;
         vertices;
         faces; 
-        stenosis_index;
-        stenosis_radius;
+        stenosis;
 
     end
     
@@ -17,11 +16,19 @@ classdef Artery
             % read in artery
             [arteryObj.faces, arteryObj.vertices, ~] = read_stl(filename_artery);
             % initialize centerline
-            arteryObj.centerline = Centerline(filename_centerline1, filename_centerline2);
+            arteryObj.centerline = get_centerlines(filename_centerline1, filename_centerline2);
+            centerline_lengths = [1,size(arteryObj.centerline(1).coords,1),size(arteryObj.centerline(2).coords,1),size(arteryObj.centerline(3).coords,1)];
+            centerline_lengths1 = cumsum(centerline_lengths);
             % calculate radii
-            [arteryObj.radii_avg, arteryObj.radii_st_dev, arteryObj.centerline.index_artery_to_center] = calc_mean_radii(arteryObj.centerline.coords, arteryObj.vertices);
-            % find stenosis
-            [arteryObj.stenosis_radius, arteryObj.stenosis_index] = min(arteryObj.radii_avg);
+            [arteryObj.radii_avg, arteryObj.radii_st_dev, arteryObj.centerline(4).index_artery_to_center] = calc_mean_radii(arteryObj.centerline(4).coords, arteryObj.vertices);
+            for i = 1:3
+                arteryObj.radii_avg(i,1:centerline_lengths(i+1)) = arteryObj.radii_avg(4,centerline_lengths1(i):centerline_lengths1(i+1)-1);
+                [radius, index] = min(arteryObj.radii_avg(1:centerline_lengths(i+1)));
+                % set Stenosis
+                stenosisObj(i) = Stenosis(radius,index);  
+            end
+            arteryObj.stenosis = stenosisObj;
+
         end
  
         
