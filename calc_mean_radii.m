4d4df7ed8a93e32741a2166390019e58c9590901
Metadelee <lina.gundelwein@yahoo.de>
2017-05-10 16:04:26 -0400

    ""

--- calc_mean_radii.m
+++ calc_mean_radii.m
@@ -1,23 +1,27 @@
 function [average, st_dev, index_artery_to_center] = calc_mean_radii(centers, artery)
+center_length = size(centers,1);
+
 % distance and index to closest centerline point for each artery point
 [dist_artery, index_center_to_artery] = pdist2(centers,artery,'euclidean','SMALLEST',1);
 index_center_to_artery = index_center_to_artery'; 
 
 % index of artery points belonging to each centerline point
-index_artery_to_center = get_index(index_center_to_artery,size(centers,1));
+index_artery_to_center = get_index(index_center_to_artery,center_length);
 
 % average after expansion
-average = zeros(size(centers,1),1);
-st_dev = zeros(size(centers,1),1);
+average = zeros(4,center_length);
+st_dev = zeros(4,size(centers,1));
 dist_artery = dist_artery';
-for i=1:size(centers,1)
+for i=1:center_length
     % number of artery points belonging to center point i
     num = sum(index_artery_to_center(i,:)~=0);
     % distances from point i to artery points
     dists = dist_artery(index_artery_to_center(i,1:num),1);
-    average(i) = sum(dists)/num;
-    st_dev(i) = std(dists);
+    average(4,i) = sum(dists)/num;
+    st_dev(4,i) = std(dists);
 end;
+
+
 
 % in case NAN appear
 % for i=2:size(centers,1)-1
