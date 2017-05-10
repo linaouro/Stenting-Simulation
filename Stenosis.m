5890e85602be33e14cfda0ced49f94eb834751c2
Metadelee <lina.gundelwein@yahoo.de>
2017-05-10 16:06:11 -0400

    "class for the stenoses"

---
+++ Stenosis.m
@@ -0,0 +1,19 @@
+classdef Stenosis
+    %   The 
+    
+    properties
+        index
+        radius
+
+    end
+    
+    methods
+        function  stenosisObj = Stenosis(radius, index)
+            stenosisObj.radius = radius;
+            stenosisObj.index = index;
+        end
+ 
+        
+    end
+    
+end
