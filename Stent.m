d9e26c16d93a3965e149107789a97cbfce382fc7
Metadelee <lina.gundelwein@yahoo.de>
2017-05-10 11:28:27 -0400

    "change constructor to initialize stent centerline"

--- Stent.m
+++ Stent.m
@@ -1,22 +1,22 @@
 classdef Stent
-    %   Summary of this class goes here
-    %   Detailed explanation goes here
+    %   Class for the patient specific 3D model of the pulmonary artery
     
     properties
         centerline;
+        radii;
         vertices;
         faces; 
         params;
-        index_artery_to_center
     end
     
     methods
-        function  stentObj = Stent(filename_stent, filename_params)
-            % read in stebt
+        function  stentObj = Stent(filename_stent, filename_params, left_center, right_center, trunk_center)
+            % read in stent
             [stentObj.faces, stentObj.vertices] = read_vertices_and_faces_from_obj_file(filename_stent, false);
             
             % initialize centerline
-            
+            stentObj.centerline = Centerline(left_center, right_center, trunk_center);
+           
             % set foreshortening parameters
             stentObj.params = csvread(filename_params);
         end
