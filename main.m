4d2fa634da2637ca3cbf409a4650a62304e92e1c
Metadelee <lina.gundelwein@yahoo.de>
2017-05-11 15:31:45 -0400

    "setup the three stents individually"

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+addpath('draw/')
 %% 1. Artery setup
 arteryObj = Artery('test_artery.stl','test_path1.pth', 'test_path2.pth');
 % display artery
@@ -10,9 +11,16 @@ draw_centerline(arteryObj.centerline)
 % draw_closest_points(arteryObj.centerline.coords,arteryObj.vertices, arteryObj.centerline.index_artery_to_center,79);
 
 %% 2. Stent setup 
-stentObj = Stent('s_stent.obj','stent_foreshortening.csv');
+stentTrunk = Stent('s_stent.obj','stent_foreshortening.csv', arteryObj.centerline(3).coords, arteryObj.centerline(3).tangents, 0.5);
+stentLeft = Stent('s_stent.obj','stent_foreshortening.csv', arteryObj.centerline(2).coords, arteryObj.centerline(2).tangents, 0.2);
+stentRight = Stent('s_stent.obj','stent_foreshortening.csv', arteryObj.centerline(1).coords, arteryObj.centerline(1).tangents, 0.2);
 
 %% 3. Intervention
+% draw stenoses
+draw_stenoses(arteryObj.stenosis);
+
 % stent expansion and foreshortening
 
+% final stent
+%arteryObj.set_stents();
 % possibly smoothing with smooth3
