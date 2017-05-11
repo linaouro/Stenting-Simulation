cb7109d104dad07cd7b75da264a1f717cf7d8ba7
Metadelee <lina.gundelwein@yahoo.de>
2017-05-11 15:32:04 -0400

    "add coordiantes to stenosis class"

--- Stenosis.m
+++ Stenosis.m
@@ -4,13 +4,16 @@ classdef Stenosis
     properties
         index
         radius
-
+        coords
     end
     
     methods
-        function  stenosisObj = Stenosis(radius, index)
-            stenosisObj.radius = radius;
-            stenosisObj.index = index;
+        function  stenosisObj = Stenosis(radius, index, coords)
+            if nargin ==3
+                stenosisObj.radius = radius;
+                stenosisObj.index = index;
+                stenosisObj.coords = coords;
+            end
         end
  
         
