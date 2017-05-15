13c7f1c07128e07a7a2c5118512997232ff69a6d
Metadelee <lina.gundelwein@yahoo.de>
2017-05-15 09:11:21 -0400

    "stent starting and end index is defined"

--- Stent.m
+++ Stent.m
@@ -10,19 +10,26 @@ classdef Stent
     end
     
     methods
-        function  stentObj = Stent(filename_stent, filename_params, coords, tangents, radius)
-            % read in stent
-            [stentObj.faces, stentObj.vertices] = read_vertices_and_faces_from_obj_file(filename_stent, false);
-            
-            % initialize centerline
-            stentObj.centerline = Centerline(coords, tangents);
-           
-            % set foreshortening parameters
-            stentObj.params = csvread(filename_params);
-            
-            stentObj.vertices = initial_alignment(stentObj.centerline, radius);
+        function  stentObj = Stent(filename_stent, filename_params, coords, tangents, radius, idx)
+            if nargin ~= 0
+                
+                % read in stent
+                %[stentObj.faces, stentObj.vertices] = read_vertices_and_faces_from_obj_file(filename_stent, false);
+
+                % initialize centerline
+                stentObj.centerline = Centerline(coords(idx(1):idx(2),:), tangents(idx(1):idx(2),:));
+
+                % set foreshortening parameters
+                stentObj.params = csvread(filename_params);
+
+                stentObj.vertices = initial_alignment(stentObj.centerline, radius, 50);
+                stentObj.faces = triangulate_stent(stentObj.vertices,50);
+                %TODO find intersection of the cylinders polyxpoly to find
+                %intersection of polylines
+            end
         end
- 
+        
+
  
     end
     
