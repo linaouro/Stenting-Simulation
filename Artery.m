129a1804b6a2efe8aa471494eb3bb5473fa0e61e
Metadelee <lina.gundelwein@yahoo.de>
2017-05-16 10:04:32 -0400

    "calc_mean_radii updated such that values are stored for each branch, plus moved to artery class"

--- Artery.m
+++ Artery.m
@@ -9,6 +9,7 @@ classdef Artery
         vertices;
         faces; 
         stenosis;
+        dist_to_center;
 
     end
     
@@ -21,21 +22,40 @@ classdef Artery
             arteryObj.centerline = get_centerlines(filename_centerline1, filename_centerline2);
             arteryObj.centerline_lengths = [1,size(arteryObj.centerline(1).coords,1)-1,size(arteryObj.centerline(2).coords,1)-1,size(arteryObj.centerline(3).coords,1)];
             
-            % calculate radii
-            arteryObj = set_radii(arteryObj);
+            % calculate radii and distances
+            arteryObj = calc_radii_dists(arteryObj);
             
             % set stenosis
             arteryObj = set_stenosis(arteryObj);
-            
- 
-
         end
 
-        function arteryObj = set_radii(arteryObj)
+        function arteryObj = calc_radii_dists(arteryObj)
+            center_length = size(arteryObj.centerline(4).coords,1);
             centerline_lengths1 = cumsum(arteryObj.centerline_lengths);
-            [arteryObj.radii_avg, arteryObj.radii_st_dev, arteryObj.centerline(4).index_artery_to_center] = calc_mean_radii(arteryObj.centerline(4).coords, arteryObj.vertices);
+            
+            % distance and index to closest centerline point for each artery point
+            [dist_art, index_center_to_artery] = pdist2(arteryObj.centerline(4).coords,arteryObj.vertices,'euclidean','SMALLEST',1);
+
+            % index of artery points belonging to each centerline point
+            arteryObj.centerline(4).index_artery_to_center = get_index(index_center_to_artery',center_length);
+
+            % average
+            arteryObj.dist_to_center(:,4) = dist_art';
+            for i=1:center_length
+                % number of artery points belonging to center point i
+                num = sum(arteryObj.centerline(4).index_artery_to_center(i,:)~=0);
+                
+                % distances from point i to artery points
+                dists = arteryObj.dist_to_center(arteryObj.centerline(4).index_artery_to_center(i,1:num),4);
+                arteryObj.radii_avg(i,4) = sum(dists)/num;
+                arteryObj.radii_st_dev(i,4) = std(dists);
+            end;
+
+            % set values for the single branches 
             for i = 1:3
-                arteryObj.radii_avg(i,1:arteryObj.centerline_lengths(i+1)) = arteryObj.radii_avg(4,centerline_lengths1(i):centerline_lengths1(i+1)-1);            
+                arteryObj.radii_avg(1:arteryObj.centerline_lengths(i+1),i) = arteryObj.radii_avg(centerline_lengths1(i):centerline_lengths1(i+1)-1,4);            
+                arteryObj.centerline(i).index_artery_to_center(1:arteryObj.centerline_lengths(i+1),:) = arteryObj.centerline(4).index_artery_to_center(centerline_lengths1(i):centerline_lengths1(i+1)-1,:);            
+                arteryObj.dist_to_center(1:arteryObj.centerline_lengths(i+1),i) = arteryObj.dist_to_center(centerline_lengths1(i):centerline_lengths1(i+1)-1,4);            
             end
         end
         
@@ -43,12 +63,14 @@ classdef Artery
             arteryObj.stenosis = [Stenosis(),Stenosis(),Stenosis()];
             offset = 10;
             for i = 1:3
-                [radius, index] = min(arteryObj.radii_avg(i,1:arteryObj.centerline_lengths(i+1)-offset));
+                [radius, index] = min(arteryObj.radii_avg(1:arteryObj.centerline_lengths(i+1)-offset,i));
                 % set Stenosis
                 arteryObj.stenosis(i) = Stenosis(radius,index, arteryObj.centerline(i).coords(index,:));  
             end
     
-        end           
+        end 
+        
+
         
     end
         
