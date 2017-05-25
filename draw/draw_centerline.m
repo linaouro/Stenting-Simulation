ab7d05f55bb53ed5ab0bfcd698560f464813b9c7
Metadelee <lina.gundelwein@yahoo.de>
2017-05-25 13:16:36 -0400

    ""

--- draw/draw_centerline.m
+++ draw/draw_centerline.m
@@ -1,4 +1,4 @@
 function draw_centerline(centerlineObj)
 for i = 1:3
-    plot3(centerlineObj(i).coords(:,1),centerlineObj(i).coords(:,2),centerlineObj(i).coords(:,3), 'Color', 'k');
+    plot3(centerlineObj(i).coords(:,1),centerlineObj(i).coords(:,2),centerlineObj(i).coords(:,3), 'Color', 'y'); hold on;
 end
