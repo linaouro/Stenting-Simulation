0114ee7c366610940705d913b6bc11be18450ca6
Metadelee <lina.gundelwein@yahoo.de>
2017-05-25 13:07:02 -0400

    "set each centerline separately up to bifurcation only trunk including bifurcation"

--- get_centerlines.m
+++ get_centerlines.m
@@ -10,34 +10,15 @@ function [centerline] = get_centerlines(filename1, filename2)
         coords = coords1;
         coords1 = coords2;
         coords2 = coords;
-%         tangents = tangents1;
-%         tangents1 = tangents2;
-%         tangents2 = tangents;
     end
-%% each centerline only few points around bifurcation
-%     [c,t,~] = interparc(5,coords1(1:3,1),coords1(1:3,2),coords1(1:3,3));
-%     centerline(1) = Centerline([c;coords1(4:end,:)], [t;tangents1(4:end,:)]); % right
-%     [c,t,~]= interparc(11,coords2(bif-1:bif+1,1),coords2(bif-1:bif+1,2),coords2(bif-1:bif+1,3));
-%     centerline(2) = Centerline([c(6:end,:);coords2(bif:end,:)],[t(6:end,:);tangents2(bif:end,:)]); % left
-%     centerline(3) = Centerline([coords2(1:bif-5,:);c(1:6,:)], [tangents2(1:bif,:);t(1:6,:)]); % trunk
 
-%% each centerline single up to bifurcation
+%% each centerline single up to bifurcation only trunk including bifurcation
     [coords,tangents,~] = interparc(size(coords1,1),coords1(:,1),coords1(:,2),coords1(:,3));
-    centerline(1) = Centerline(coords,tangents); % right
+    centerline(1) = Centerline(coords(2:end,:),tangents(2:end,:)); % right
     [coords,tangents,~]= interparc(size(coords2(bif:end,:),1),coords2(bif:end,1),coords2(bif:end,2),coords2(bif:end,3));
-    centerline(2) = Centerline(coords,tangents); % left
+    centerline(2) = Centerline(coords(2:end,:),tangents(2:end,:)); % left
     [coords,tangents,~]= interparc(size(coords2(1:bif,:),1),coords2(1:bif,1),coords2(1:bif,2),coords2(1:bif,3));
     centerline(3) = Centerline(coords(end:-1:1,:),tangents(end:-1:1,:)); % trunk
 
-% %% two big centerlines plus trunk
-%     [coords,tangents,~] = interparc(size(coords1,1)+size(coords2(1:bif-1,:),1),[coords2(1:bif-1,1);coords1(:,1)],[coords2(1:bif-1,2);coords1(:,2)],[coords2(1:bif-1,3);coords1(:,3)]);
-%     centerline(1) = Centerline(coords,tangents); % right
-%     [coords,tangents,~]= interparc(size(coords2,1),coords2(:,1),coords2(:,2),coords2(:,3));
-%     centerline(2) = Centerline(coords,tangents); % left
-%     %[coords,tangents,~]= interparc(size(coords2(1:bif,:),1),coords2(1:bif,1),coords2(1:bif,2),coords2(1:bif,3));
-%     centerline(3) = Centerline(coords2(1:bif-1,:),tangents2(1:bif,:)); % trunk    
 
-    %centerline(1).tangents(1,:) = mean([centerline(1).tangents(1,:); centerline(2).tangents(1,:);centerline(3).tangents(end-1,:)],1);
-    %centerline(2).tangents(1,:) = centerline(1).tangents(1,:);
-    %centerline(3).tangents(end,:) = centerline(1).tangents(1,:);
-    centerline(4) = Centerline([centerline(1).coords(2:end,:); centerline(2).coords(2:end,:); centerline(3).coords],[centerline(1).tangents(2:end,:); centerline(2).tangents(2:end,:); centerline(3).tangents]);
+    centerline(4) = Centerline([centerline(1).coords; centerline(2).coords; centerline(3).coords],[centerline(1).tangents; centerline(2).tangents; centerline(3).tangents]);
