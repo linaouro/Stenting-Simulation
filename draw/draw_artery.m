ed2889df7b90956f8ca07a10f7f2ef46ee97331c
Metadelee <lina.gundelwein@yahoo.de>
2017-06-14 09:09:47 -0400

    "update lightning for smoother appearance real stent mesh "

--- draw/draw_artery.m
+++ draw/draw_artery.m
@@ -1,12 +1,15 @@
 function f=draw_artery(arteryObj)
-    f = figure;
+    %f = figure;
+    f=1;
     clf;
     p = patch('faces', arteryObj.faces, 'vertices' ,arteryObj.vertices);
     set(p, 'facec', 'r');              % Set the face color (force it)
-    set(p, 'facealpha',.7)             % Use for transparency
+    set(p, 'facealpha',.4)             % Use for transparency
     set(p, 'EdgeColor','none');         % Set the edge color
     %set(p, 'EdgeColor',[1 0 0 ]);      % Use to see triangles, if needed.
-    light                               % add a default light
+    light('Position',[-1.0,-1.0,100.0],'Style','infinite');
+    lighting gouraud;
+    axis equal;
     daspect([1 1 1])                    % Setting the aspect ratio
     view([230 60])                      
     xlabel('X'),ylabel('Y'),zlabel('Z')
