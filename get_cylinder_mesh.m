e8bed0dda44109f0afc162be2c55f3bf7295cc28
Metadelee <lina.gundelwein@yahoo.de>
2017-06-14 09:28:40 -0400

    "remove unnecessary argument nx correct y coordinate"

--- get_cylinder_mesh.m
+++ get_cylinder_mesh.m
@@ -1,16 +1,16 @@
-function [rr, vertices, faces] = get_cylinder_mesh(centerline, nx, nz, radii )
+function [rr, vertices, faces] = get_cylinder_mesh(centerline, nz, radii )
 % modified CYLINDERMESH function from geom3d
 % Create a 3D mesh representing a cylinder curved along centerline
 %
 %   [V F] = cylinderMesh(CYL)
 %   Computes vertex coordinates and face vertex indices of a mesh
 
+global n_circ;
 % compute length and orientation
 coords = centerline.coords;
 tangents = centerline.tangents;
-seglen = [0; (centerline.seglen)];
 % parametrisation on x
-nx = nx+1;
+nx = n_circ+1;
 t = linspace(0, 2*pi, nx);
 x = zeros(nz,nx);
 y = zeros(nz,nx);
@@ -25,7 +25,7 @@ for i=1:nz
     rr(1+(i-1)*(nx-1):(nx-1)*i,:) = radii(i) * ones(nx-1,1);
     % rotation of circle
     [theta, phi, ~] = cart2sph2d(tangents(i,:));
-    lz = ones(size(lx))*seglen(i);
+    lz = zeros(size(lx));%ones(size(lx))*seglen(i);
     trans   = localToGlobal3d(coords(i,:), theta, phi, 0);
     [x(i,:), y(i,:), z(i,:)] = transformPoint3d(lx', ly', lz', trans);
 end
