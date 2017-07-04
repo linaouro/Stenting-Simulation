9a2e79d10e00d6b743393e4c56ca1180ac900a7b
Metadelee <lina.gundelwein@yahoo.de>
2017-07-04 11:35:21 -0400

    "simple and connected version"

--- draw/draw_centerline.m
+++ draw/draw_centerline.m
@@ -1,4 +1,9 @@
 function draw_centerline(centerlineObj)
+%stars
+% for i = 1:3
+%     plot3(centerlineObj(i).coords(:,1),centerlineObj(i).coords(:,2),centerlineObj(i).coords(:,3),'*-', 'LineWidth', 1); hold on;
+% end
+%simple
 for i = 1:3
-    plot3(centerlineObj(i).coords(:,1),centerlineObj(i).coords(:,2),centerlineObj(i).coords(:,3), 'LineWidth', 2, 'Color', 'b'); hold on;
+    plot3(centerlineObj(i).coords(:,1),centerlineObj(i).coords(:,2),centerlineObj(i).coords(:,3),'LineWidth', 1, 'Color', 'b'); hold on;
 end
